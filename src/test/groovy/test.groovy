9bd532b5382287f87102a9e316ce2f1e105db1fc
Sreejith M <55193@hexaware.com>
2021-09-27 10:56:40 +0000

    Updated test.groovy

--- src/test/groovy/test.groovy
+++ src/test/groovy/test.groovy
@@ -1,8 +1,11 @@
 @Library('shared-lib-unitTesting')_
+
 import org.junit.Test
+import org.junit.Before
 import com.lesfurets.jenkins.unit.*
 import static groovy.test.GroovyAssert.*
 import static org.junit.Assert.assertEquals
+
 class SimpleUnitTest extends BasePipelineTest {
    def temp
    
