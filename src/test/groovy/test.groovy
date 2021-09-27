7f8f935d103192ed2f18da5db2a6340777710891
Sreejith M <55193@hexaware.com>
2021-09-27 10:53:11 +0000

    Added test.groovy

---
+++ src/test/groovy/test.groovy
@@ -0,0 +1,21 @@
+@Library('shared-lib-unitTesting')_
+import org.junit.Test
+import com.lesfurets.jenkins.unit.*
+import static groovy.test.GroovyAssert.*
+import static org.junit.Assert.assertEquals
+class SimpleUnitTest extends BasePipelineTest {
+   def temp
+   
+   @Before
+    void setUp() {
+        super.setUp()
+        // load temp
+        temp = loadScript("vars/temp.groovy")
+
+   @Test
+    void Temperatureconverter() {
+        // call temp and check result
+        def result = temp(95)
+        assertEquals(35, result)
+    }
+}
