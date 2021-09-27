95121bf3ad94200e9ac23692e3121c5080da5c25
Sreejith M <55193@hexaware.com>
2021-09-27 14:24:03 +0000

    Updated test.groovy

--- src/test/groovy/test.groovy
+++ src/test/groovy/test.groovy
@@ -7,21 +7,21 @@ import static groovy.test.GroovyAssert.*
 import static org.junit.Assert.assertEquals
 
 class SimpleUnitTest extends BasePipelineTest {
-   def temp
+   def t
    
    @Before
     void setUp() {
         super.setUp()
         // load temp
-        def t = library 'shared-lib-unitTesting' 
-        
+        def t = library 'shared-lib-unitTesting'.t.vars.temp(98)
+        //temp = t.vars.temp(98)
+
     }
 
    @Test
     void Temperatureconverter() {
         // call temp and check result
         //def result = temp(98)
-        temp = t.vars.temp(98)
-        assert 36.6666666667 == temp
+        assert 36.6666666667 == t
     }
 }
